d25047219073ba27f6cf1016a1caba5537a83300
dillonzellis <dillonzellis@gmail.com>
2024-01-30 14:09:07 -0500

    feat: modify BodyText to take variants

--- src/components/typography/BodyText.tsx
+++ src/components/typography/BodyText.tsx
@@ -1,10 +1,18 @@
+import { cn } from "@/lib/utils";
+
 interface BodyTextProps {
   children: React.ReactNode;
+  className?: string;
 }
 
-export const BodyText = ({ children }: BodyTextProps) => {
+export const BodyText = ({ children, className }: BodyTextProps) => {
   return (
-    <div className="font-light text-lg tracking-tighter text-muted-foreground leading-8">
+    <div
+      className={cn(
+        "font-light text-lg tracking-tighter text-muted-foreground leading-8",
+        className,
+      )}
+    >
       {children}
     </div>
   );
