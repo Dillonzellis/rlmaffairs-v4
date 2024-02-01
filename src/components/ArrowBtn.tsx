5798033f4ba8f66567ad4990bca582d6a21ff845
dillonzellis <dillonzellis@gmail.com>
2024-02-01 14:31:15 -0500

    fix: add missing svg to Arrow btn

--- src/components/ArrowBtn.tsx
+++ src/components/ArrowBtn.tsx
@@ -1,4 +1,5 @@
 import { Button } from "./ui/button";
+import Image from "next/image";
 import Link from "next/link";
 
 interface ArrowBtnProps {
@@ -8,14 +9,23 @@ interface ArrowBtnProps {
 
 export const ArrowBtn = ({ href, children }: ArrowBtnProps) => {
   return (
-    <Button
-      asChild
-      variant="link"
-      size="none"
-      fontSize="lg"
-      className="font-light"
-    >
-      <Link href={href}>{children}</Link>
-    </Button>
+    <div className="flex items-center gap-4 pb-8 md:gap-8 md:pb-0">
+      <Image
+        src="/right-arr.svg"
+        alt=""
+        width={58}
+        height={20}
+        className="h-3.5 w-10 md:h-5 md:w-14"
+      />
+      <Button
+        asChild
+        variant="link"
+        size="none"
+        fontSize="lg"
+        className="font-light"
+      >
+        <Link href={href}>{children}</Link>
+      </Button>
+    </div>
   );
 };
