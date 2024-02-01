0510e01287f6e1d892c14253accc4bb200d8b071
dillonzellis <dillonzellis@gmail.com>
2024-02-01 00:00:30 -0500

    style: add footer bg image

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,10 +1,14 @@
+import Image from "next/image";
 import { FooterCTA } from "./FooterCTA";
 
 export const Footer = () => {
   return (
-    <footer>
+    <footer className="bg-[#07111F]">
       <FooterCTA />
       <div>Footer links</div>
+      <div>
+        <Image src="/footer-bg.png" alt="logo" width={1280} height={517} />
+      </div>
     </footer>
   );
 };
