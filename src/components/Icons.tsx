a2c7353818bc12b436b7dc90849fd3196c01452f
dillonzellis <dillonzellis@gmail.com>
2024-02-10 13:02:31 -0500

    feat: add Icons component

---
+++ src/components/Icons.tsx
@@ -0,0 +1,26 @@
+import { LucideProps } from "lucide-react";
+
+export const Icons = {
+  downArrow: (props: LucideProps) => (
+    <svg {...props} viewBox="0 0 20 55">
+      <g clip-path="url(#clip0_731_303)">
+        <path
+          fill-rule="evenodd"
+          clip-rule="evenodd"
+          d="M20 43.3L10 55L9.53674e-06 43.3L0.900009 42.4L9.30001 52.2L9.30001 0L10.7 0L10.7 52.2L19.1 42.4L20 43.3Z"
+          fill="#FCFCFC"
+        />
+      </g>
+      <defs>
+        <clipPath id="clip0_731_303">
+          <rect
+            width="55"
+            height="20"
+            fill="white"
+            transform="matrix(0 1 -1 0 20 0)"
+          />
+        </clipPath>
+      </defs>
+    </svg>
+  ),
+};
