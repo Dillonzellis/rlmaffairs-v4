3a218267d27a647c11badbf1e464e4d0756a1d7a
dillonzellis <dillonzellis@gmail.com>
2024-04-17 15:39:37 -0400

    style: update venue img hero

--- src/app/venue-&-event-space/page.tsx
+++ src/app/venue-&-event-space/page.tsx
@@ -1,15 +1,12 @@
 import { PageHero } from "@/components/common/PageHero";
 import { VenuePackages } from "@/components/sections/venue/VenuePackages";
 import { VenueHashLinks } from "@/data/navLinks";
+import venueHero from "/public/venue-hero.jpg";
 
 export default function VenuePage() {
   return (
     <main>
-      <PageHero
-        imgSrc="/hero-placeholder.jpg"
-        imgAlt=""
-        hashLinks={VenueHashLinks}
-      />
+      <PageHero imgSrc={venueHero} imgAlt="" hashLinks={VenueHashLinks} />
 
       {/* Packages */}
       <VenuePackages />
