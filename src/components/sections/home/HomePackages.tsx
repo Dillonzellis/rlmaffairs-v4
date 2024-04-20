7b02e3026c04e5fe18504db0d76df04a6990f92a
dillonzellis <dillonzellis@gmail.com>
2024-04-20 12:39:48 -0400

    feat: add links to home page package btns

--- src/components/sections/home/HomePackages.tsx
+++ src/components/sections/home/HomePackages.tsx
@@ -2,15 +2,17 @@ import { BackgroundImage } from "@/components/common/BackgroundImage";
 import { Icons } from "@/components/common/Icons";
 import { MaxWidthWrapper } from "@/components/common/MaxWidthWrapper";
 import { cn } from "@/lib/utils";
+import Link from "next/link";
 
 type PackageBtnProps = {
+  href: string;
   children: React.ReactNode;
   green?: boolean;
 };
 
-const PackageBtn = ({ children, green }: PackageBtnProps) => {
+const PackageBtn = ({ href, children, green }: PackageBtnProps) => {
   return (
-    <div className="max-w-xl">
+    <Link href={href} className="max-w-xl">
       <div
         className={cn(
           "item-center inline-flex h-[21rem] cursor-pointer flex-col justify-center rounded-2xl border-b-[7px] px-8 py-4 font-serif text-9xl text-white transition-colors active:border-b-0",
@@ -22,7 +24,7 @@ const PackageBtn = ({ children, green }: PackageBtnProps) => {
         {children}
         <Icons.lgRightArrow className="" />
       </div>
-    </div>
+    </Link>
   );
 };
 
@@ -31,8 +33,10 @@ export const HomePackages = () => {
     <section>
       <MaxWidthWrapper className="py-12">
         <div className="flex flex-col justify-center gap-16 md:flex-row">
-          <PackageBtn>Wedding Packages</PackageBtn>
-          <PackageBtn green>Event Packages</PackageBtn>
+          <PackageBtn href="/wedding-packages">Wedding Packages</PackageBtn>
+          <PackageBtn href="/event-packages" green>
+            Event Packages
+          </PackageBtn>
         </div>
       </MaxWidthWrapper>
       <BackgroundImage imageUrl="venue-1.jpg" />
