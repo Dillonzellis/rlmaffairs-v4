779f8af05ea79e3786861615c438cf7121a2d681
dillonzellis <dillonzellis@gmail.com>
2024-05-02 14:51:56 -0400

    feat: our story rough with accordion

--- src/app/our-story/page.tsx
+++ src/app/our-story/page.tsx
@@ -1,3 +1,38 @@
+import { MaxWidthWrapper } from "@/components/common/MaxWidthWrapper";
+import { MainHeader } from "@/components/common/typography/MainHeader";
+import { FAQAccordion } from "@/components/sections/our-story/FAQAccordion";
+
 export default function OurStoryHome() {
-  return <main>Our STory</main>;
+  return (
+    <main>
+      <section>
+        <div className="h-[350px] bg-[url('/package-feat-sm.jpg')] bg-cover bg-center">
+          <MaxWidthWrapper className="flex h-full items-center">
+            <MainHeader className="text-white">Our Story</MainHeader>
+          </MaxWidthWrapper>
+        </div>
+      </section>
+      <section className="py-12">
+        <MaxWidthWrapper>
+          <div>About Us</div>
+        </MaxWidthWrapper>
+      </section>
+      <section className="py-12">
+        <MaxWidthWrapper>
+          <div>FAQ</div>
+          <FAQAccordion />
+        </MaxWidthWrapper>
+      </section>
+      <section className="py-12">
+        <MaxWidthWrapper>
+          <div>Contact</div>
+        </MaxWidthWrapper>
+      </section>
+      <section className="py-12">
+        <MaxWidthWrapper>
+          <div>Google Maps</div>
+        </MaxWidthWrapper>
+      </section>
+    </main>
+  );
 }
