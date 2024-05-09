3c0b85022a1a44f2089fb2c1796df82c990c4b78
dillonzellis <dillonzellis@gmail.com>
2024-05-09 11:50:18 -0400

    style: make home packages mobile responsive

--- src/components/sections/home/HomePackages.tsx
+++ src/components/sections/home/HomePackages.tsx
@@ -15,14 +15,14 @@ const PackageBtn = ({ href, children, green }: PackageBtnProps) => {
     <Link href={href} className="max-w-xl">
       <div
         className={cn(
-          "item-center inline-flex h-[21rem] cursor-pointer flex-col justify-center rounded-2xl border-b-[7px] px-8 py-4 font-serif text-9xl text-white transition-colors active:border-b-0",
+          "item-center inline-flex cursor-pointer flex-col justify-center rounded-2xl border-b-[7px] px-6 pb-3 pt-4 text-center font-serif text-3xl text-white transition-colors active:border-b-0 active:pt-3 md:h-[21rem] md:px-8 md:py-4 md:text-9xl",
           green
             ? "border-b-brandGreen-800 bg-brandGreen-400 hover:bg-brandGreen-300"
             : "border-b-brandGold-800 bg-brandGold-400 hover:bg-brandGold-300",
         )}
       >
         {children}
-        <Icons.lgRightArrow className="" />
+        <Icons.lgRightArrow className="hidden md:w-[10.5rem]" />
       </div>
     </Link>
   );
@@ -32,7 +32,7 @@ export const HomePackages = () => {
   return (
     <section>
       <MaxWidthWrapper className="py-12">
-        <div className="flex flex-col justify-center gap-16 md:flex-row">
+        <div className="flex flex-col items-center justify-center gap-4 md:flex-row md:gap-16">
           <PackageBtn href="/wedding-packages">Wedding Packages</PackageBtn>
           <PackageBtn href="/event-packages" green>
             Event Packages
