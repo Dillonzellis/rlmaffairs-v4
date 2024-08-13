6ff33d07b5bf6e4eb1934b63d74695dba18d99eb
dillonzellis <dillonzellis@gmail.com>
2024-08-13 19:03:25 -0400

    fix background fixed on mobile

--- src/components/common/BackgroundImage.tsx
+++ src/components/common/BackgroundImage.tsx
@@ -5,7 +5,7 @@ interface BackgroundImageProps {
 export const BackgroundImage = ({ imageUrl }: BackgroundImageProps) => {
   return (
     <div
-      className={`min-h-72 bg-cover bg-fixed bg-center bg-no-repeat md:min-h-[573px]`}
+      className={`min-h-72 bg-cover bg-center bg-no-repeat md:min-h-[573px] md:bg-fixed`}
       style={{ backgroundImage: `url(${imageUrl})` }}
     ></div>
   );
