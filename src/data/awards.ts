554ccb84c1365bca42bb935f534b9efe9b608732
dillonzellis <dillonzellis@gmail.com>
2024-08-13 19:26:28 -0400

    update awards in footer

--- src/data/awards.ts
+++ src/data/awards.ts
@@ -1,6 +1,6 @@
 export const awards = [
-  { src: "/knot-2020.png", alt: "" },
-  { src: "/knot-2020.png", alt: "" },
-  { src: "/knot-2020.png", alt: "" },
-  { src: "/knot-2020.png", alt: "" },
+  { src: "/knot-2020.png", alt: "2020 knot award" },
+  { src: "/knot2021.png", alt: "2021 knot award" },
+  { src: "/knot2022.webp", alt: "2022 knot award" },
+  { src: "/wedding-wire-2023.png", alt: "2023 wedding wire award" },
 ] as const;
