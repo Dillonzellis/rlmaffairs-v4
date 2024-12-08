66cc8956911c87863d23b4739b948e54a59874d7
Dillon Ellis <dillonzellis@gmail.com>
2024-12-08 12:54:48 -0500

    add address and phone to footer

--- src/components/layout/footer/FooterCTA.tsx
+++ src/components/layout/footer/FooterCTA.tsx
@@ -1,6 +1,6 @@
-import Image from "next/image";
-import { Button } from "@/components/common/ui/button";
 import { MaxWidthWrapper } from "@/components/common/MaxWidthWrapper";
+import { Button } from "@/components/common/ui/button";
+import Image from "next/image";
 import Link from "next/link";
 
 export const FooterCTA = () => {
@@ -21,6 +21,22 @@ export const FooterCTA = () => {
                 Book A Tour!
               </Link>
             </Button>
+          </div>
+          <div className="flex flex-col items-center justify-center gap-1.5 md:text-lg">
+            <div>
+              <a
+                className="hover:underline"
+                href="https://www.google.com/maps/place/RLM+Affairs+Wedding+Venue/@34.2644018,-83.8879582,17z/data=!4m6!3m5!1s0x88f5f30b4abeaadb:0xc36e0382d9c6cd07!8m2!3d34.2643974!4d-83.8853833!16s%2Fg%2F11j37qrzvv?entry=ttu&g_ep=EgoyMDI0MTIwNC4wIKXMDSoASAFQAw%3D%3D"
+                target="_blank"
+              >
+                3575 McEver Rd, Gainesville, GA 30504
+              </a>
+            </div>
+            <div>
+              <a className="hover:underline" href="tel:+17705367306">
+                (770)-536-7306
+              </a>
+            </div>
           </div>
         </MaxWidthWrapper>
       </div>
