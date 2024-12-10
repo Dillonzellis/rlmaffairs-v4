9f346c30b0ac0fdd40ce6c87863e52d84e991cd7
dillonzellis <dillonzellis@gmail.com>
2024-12-09 19:34:04 -0500

    clean up homeAbout

--- src/components/sections/home/HomeAbout.tsx
+++ src/components/sections/home/HomeAbout.tsx
@@ -1,25 +1,24 @@
 import { MaxWidthWrapper } from "@/components/common/MaxWidthWrapper";
 import { MainHeader } from "@/components/common/typography/MainHeader";
 import { BodyText } from "@/components/common/typography/BodyText";
+import { SubHeader } from "@/components/common/typography/SubHeader";
 
 export const HomeAbout = () => {
   return (
     <section>
       <MaxWidthWrapper className="">
-        <div className="flex flex-col gap-12 py-8 md:flex-row">
-          <div>
-            <MainHeader size="medium">RLM Affairs Wedding Venue</MainHeader>
-            <BodyText className="">
-              RLM Affairs offers all the services you may need for your event.
-              Our staff works directly with our clients to tailor each event to
-              your specific needs. Whether you need just catering, or a complete
-              event design, we will work with you as much as you need, or we
-              will provide you with A la carte services based specifically on
-              your needs and budget. Our award winning catering, wedding
-              planning, cakes, and floral design are available every day,
-              breakfast, lunch, dinner, or in between.
-            </BodyText>
-          </div>
+        <div className="flex flex-col gap-4 py-12">
+          <MainHeader size="medium">RLM Affairs Wedding Venue</MainHeader>
+          <BodyText className="">
+            RLM Affairs offers all the services you may need for your event. Our
+            staff works directly with our clients to tailor each event to your
+            specific needs. Whether you need just catering, or a complete event
+            design, we will work with you as much as you need, or we will
+            provide you with A la carte services based specifically on your
+            needs and budget. Our award winning catering, wedding planning,
+            cakes, and floral design are available every day, breakfast, lunch,
+            dinner, or in between.
+          </BodyText>
         </div>
       </MaxWidthWrapper>
 
