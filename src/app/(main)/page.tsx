3d933221b323c08af304f513dd0d4d3a63952452
dillonzellis <dillonzellis@gmail.com>
2024-12-09 19:18:37 -0500

    trying new h1

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -5,17 +5,18 @@ import { LoveLetters } from "@/components/sections/home/LoveLetters";
 import { PageHero } from "@/components/common/PageHero";
 import { HomePackages } from "@/components/sections/home/HomePackages";
 import { HomeAbout } from "@/components/sections/home/HomeAbout";
+import { HomeRlm } from "@/components/sections/home/HomeRlm";
 
 export default function Home() {
   return (
     <main className="min-h-dvh">
       <PageHero videoSrc="/RLM-Website.webm" link="#venue" />
+      <HomeAbout />
       <HomeVenue />
       <HomeFloral />
       <HomePackages />
       <HomeCatering />
       <LoveLetters />
-      <HomeAbout />
     </main>
   );
 }
--- src/components/sections/home/HomeAbout.tsx
+++ src/components/sections/home/HomeAbout.tsx
@@ -6,31 +6,25 @@ import { BodyText } from "@/components/common/typography/BodyText";
 export const HomeAbout = () => {
   return (
     <section>
-      <div className="h-[350px] bg-[url('/home-story-hero.jpg')] bg-cover bg-[2rem]">
-        <MaxWidthWrapper className="flex h-full items-center">
-          <MainHeader className="text-white">Our Story</MainHeader>
-        </MaxWidthWrapper>
-      </div>
       <MaxWidthWrapper className="">
         <div className="flex flex-col items-center justify-center gap-12 py-8 md:flex-row">
-          <Image
-            src="/RLMAffairs_Logo_Gold_FNL.png"
-            alt="RLM Affairs Logo"
-            width={200}
-            height={200}
-          />
-          <BodyText>
-            RLM Affairs offers all the services you may need for your event. Our
-            staff works directly with our clients to tailor each event to your
-            specific needs. Whether you need just catering, or a complete event
-            design, we will work with you as much as you need, or we will
-            provide you with A la carte services based specifically on your
-            needs and budget. Our award winning catering, wedding planning,
-            cakes, and floral design are available every day, breakfast, lunch,
-            dinner, or in between.
-          </BodyText>
+          <div>
+            <MainHeader>RLM Affairs Wedding Venue</MainHeader>
+            <BodyText className="max-w-prose">
+              RLM Affairs offers all the services you may need for your event.
+              Our staff works directly with our clients to tailor each event to
+              your specific needs. Whether you need just catering, or a complete
+              event design, we will work with you as much as you need, or we
+              will provide you with A la carte services based specifically on
+              your needs and budget. Our award winning catering, wedding
+              planning, cakes, and floral design are available every day,
+              breakfast, lunch, dinner, or in between.
+            </BodyText>
+          </div>
         </div>
       </MaxWidthWrapper>
+
+      <div className="h-[350px] bg-[url('/home-story-hero.jpg')] bg-cover bg-[2rem]"></div>
     </section>
   );
 };
