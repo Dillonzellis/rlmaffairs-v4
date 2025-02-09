e100627e5679761b2fccb8be224b674bc56b022a
Dillon Ellis <31318165+Dillonzellis@users.noreply.github.com>
2025-02-09 18:45:36 -0500

    add VenueTour to Venue page

---
+++ src/components/sections/venue/VenueTour.tsx
@@ -0,0 +1,28 @@
+import { MaxWidthWrapper } from "@/components/common/MaxWidthWrapper";
+import { MainHeader } from "@/components/common/typography/MainHeader";
+import { SubHeader } from "@/components/common/typography/SubHeader";
+
+export const VenueTour = () => {
+  return (
+    <>
+      <section id="tour" className="scroll-mt-[4.25rem]">
+        <MaxWidthWrapper className="py-12">
+          <SubHeader>Premier Packages</SubHeader>
+          <MainHeader>Virtual Tour</MainHeader>
+        </MaxWidthWrapper>
+        <MaxWidthWrapper>
+          <div className="aspect-video">
+            <iframe
+              className="h-full w-full"
+              src="https://www.youtube.com/embed/4Umn9ClORJc?si=bfSqBGJywYrPABH9"
+              title="YouTube video player"
+              frameBorder="0"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+              allowFullScreen
+            />
+          </div>
+        </MaxWidthWrapper>
+      </section>
+    </>
+  );
+};
