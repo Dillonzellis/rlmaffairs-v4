df933ac6ffb06f4ecda03e3f08e51b11b986c3b4
dillonzellis <31318165+Dillonzellis@users.noreply.github.com>
2026-07-14 09:43:36 -0400

    add feature disclaimer

--- src/components/sections/venue/VenuePackages.tsx
+++ src/components/sections/venue/VenuePackages.tsx
@@ -9,6 +9,9 @@ export const VenuePackages = () => {
         <MaxWidthWrapper className="py-12">
           <SubHeader>Premier Packages</SubHeader>
           <MainHeader>Venue Features</MainHeader>
+          <div className="text-sm font-light italic tracking-tighter text-primary/70">
+            Features depend upon chosen package and additional services added.
+          </div>
         </MaxWidthWrapper>
       </section>
     </>
